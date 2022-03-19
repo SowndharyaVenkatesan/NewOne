4a2c076696b5db63136e4cc9c45614ced1a4ab56
SowndharyaVenkatesan <vsowndharyasona@gmail.com>
2022-03-19 10:40:00 +0530

    Second Commit - Master

--- src/main/java/com/test/divya/DivyaApplication.java
+++ src/main/java/com/test/divya/DivyaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DivyaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Tests");
 		SpringApplication.run(DivyaApplication.class, args);
 	}
 
